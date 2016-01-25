570b882f53cb891b3c0249f7ca085970f5d5a303
Shalabh Aggarwal <shalabh7777@gmail.com>
2016-01-25 15:26:42 +0530

    Part 1 - Essentials

--- my_app.py
+++ my_app.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
+PRODUCTS = {
+    'iphone': {
+        'name': 'iPhone 5S',
+        'category': 'Phones',
+        'price': 699,
+    },
+    'galaxy': {
+        'name': 'Samsung Galaxy 5',
+        'category': 'Phones',
+        'price': 649,
+    },
+    'ipad-air': {
+        'name': 'iPad Air',
+        'category': 'Tablets',
+        'price': 649,
+    },
+    'ipad-mini': {
+        'name': 'iPad Mini',
+        'category': 'Tablets',
+        'price': 549
+    }
+}
+
 @app.route('/')
-@app.route('/hello')
-@app.route('/hello/<user>')
-def hello_world(user=None):
-    user = user or 'Shalabh'
-    return render_template('index.html', user=user)
+@app.route('/home')
+def home():
+    return render_template('home.html', products=PRODUCTS)
+
+@app.route('/product/<key>')
+def product(key):
+    product = PRODUCTS.get(key)
+    if not product:
+        abort(404)
+    return render_template('product.html', product=product)
